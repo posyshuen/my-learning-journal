2d7942f42f1d80bd3e807785660c75c19c428796
Posy <posyshuen@gmail.com>
2025-07-25 17:35:04 +0800

    progress

---
+++ js/blog-data.js
@@ -0,0 +1,44 @@
+export const blogPosts = [
+	{
+		title: "Blog title one",
+		date: "July 24, 2025",
+		image: "images/blog/coding.png",
+		preview:
+			"I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning HTML, CSS, and JavaScript, I'm ready to dive into the world of web development. This blog will document my progress and share insights along the way.",
+	},
+	{
+		title: "Blog title two",
+		date: "July 25, 2025",
+		image: "images/blog/desert.png",
+		preview:
+			"Today was the first day of the Scrimba Bootcamp. We covered the basics of JavaScript, including variables, data types, and functions. The interactive coding challenges were a great way to reinforce what I learned. I'm looking forward to building more complex projects as the bootcamp progresses.",
+	},
+	{
+		title: "Blog title three",
+		date: "July 26, 2025",
+		image: "images/blog/light-bulb.png",
+		preview:
+			"I spent the day working on JavaScript arrays and loops. I learned how to manipulate arrays using methods like push, pop, and map. Loops helped me iterate through data efficiently. It's amazing how much more I can do with JavaScript now that I understand these concepts better.",
+	},
+	{
+		title: "Blog title four",
+		date: "July 27, 2025",
+		image: "images/blog/macbook.png",
+		preview:
+			"I'm starting to feel more comfortable with JavaScript syntax. Today I worked on a project that involved creating a simple calculator. It was challenging but rewarding to see it come together.",
+	},
+	{
+		title: "Blog title five",
+		date: "July 28, 2025",
+		image: "images/blog/purple-sky.png",
+		preview:
+			"I've been exploring the concept of asynchronous programming in JavaScript. It's fascinating how promises and async/await can simplify code that involves API calls or time-consuming tasks.",
+	},
+	{
+		title: "Blog title six",
+		date: "July 29, 2025",
+		image: "images/blog/react.png",
+		preview:
+			"Today I learned about JavaScript arrays and objects. They are powerful data structures that allow me to store and manipulate collections of data efficiently. I'm excited to apply this knowledge in my upcoming projects.",
+	},
+]
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import blogPosts from "./blog-data.js"
+
+const blogImg = document.querySelector(".blog-img")
